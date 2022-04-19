45d4daf82e2e931c7d76e17ce147116995a6a736
eshaanmandal <eshaanmandal29@gmailcom>
2022-04-19 13:22:33 +0530

    added convolution helper functions about and help menu remains to be added

--- helpers.py
+++ helpers.py
@@ -4,8 +4,91 @@ import numpy as np
 
 def load_image(path_to_image: str):
     try:
-        return (True, cv.cvtColor(cv.imread(path_to_image), cv.COLOR_BGR2GRAY))
-    except cv.error as error:
-        return (False, np.zeros(2))
+        img = cv.imread(path_to_image)
+        if len(img.shape) == 3:
+            channel = 3
+            img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
+        else:
+            channel = 1
     
+        return (channel, True, img)
+
+    except cv.error as error:
+        return (False, None)
+
+why = '''
+
+'''
+how = '''
+
+'''
+about_point_ops = '''
+
+'''
+about_filters = '''
+
+'''
+
+
+def help_menu():
+    '''Help message for using the program and also prints info about all the filters and point operations'''
+    while True:
+        message = '''
+        1. Why this program? (w/W)
+        2. How to use this program? (h/H)
+        3. About point operations (p/P)
+        4. About filters (f/F)
+        5. Go back to previous menu (b/B)
+    '''
+        print(message)
+        choice = input("Choice : ").lower()
+        if choice == "w":
+            print(why)
+        elif choice == "h":
+            print(how)
+        elif choice == "p":
+            print(about_point_ops)
+        elif choice == "f":
+            print(about_filters)
+        elif choice == "b":
+            return
+        else:
+            print("Invalid choice")
+    
+    
+def about():
+    about_message = '''
+    __author__ = "Eshaan Mandal"
+    __email__ = "eshaanmandal29@gmail.com"
+    '''
+    return about_message
+
+def convolve(X : np.array, F: np.array):
+    # height and width of the image
+    X_height = X.shape[0]
+    X_width = X.shape[1]
+    
+    # height and width of the filter
+    F_height = F.shape[0]
+    F_width = F.shape[1]
+    
+    H = (F_height - 1) // 2
+    W = (F_width - 1) // 2
+    
+    #output numpy matrix with height and width
+    out = np.zeros((X_height, X_width))
+    #iterate over all the pixel of image X
+    for i in np.arange(H, X_height-H):
+        for j in np.arange(W, X_width-W):
+            sum = 0
+            #iterate over the filter
+            for k in np.arange(-H, H+1):
+                for l in np.arange(-W, W+1):
+                    #get the corresponding value from image and filter
+                    a = X[i+k, j+l]
+                    w = F[H+k, W+l]
+                    sum += (w * a)
+            out[i,j] = sum
+    #return convolution  
+    return out
     
